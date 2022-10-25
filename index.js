bdcb1c7c87d324e3ccab7a267414d592d19386db
DelacroixRossin <delacroixrossin01@gmail.com>
2022-10-25 16:10:52 +0200

    first part

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express= require('express');
+const express = require("express");
+const app = express();
+const path = require('path')
 
-const app= express();
+app.set("view engine", "ejs");
 
 
-app.listen(8000, ()=>{
-    console.log('serveur est a l ecoute sur le port 8000')
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname + '/public')))
+
+console.log((path.join(__dirname + '/public')))
+
+app.get("/", (req, res) => {
+  res.render('index');
 })
+
+app.listen(4000, () => {
+  console.log("Serveur démarré au port 4000 !");
+});
